d82c928af606244bc4a478ad282954a371ef6094
mitufan <ksenonproject874@gmail.com>
2024-09-27 10:28:47 +0300

    RIBA'CH 1.2

--- board.php
+++ board.php
@@ -111,7 +111,7 @@
 
 <html>
 	<head>
-		<title>OpenOne'ch</title>
+		<title>RIBA'CH</title>
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
 		<link rel="stylesheet" href="css.css">
